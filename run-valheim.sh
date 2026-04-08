0184872a99cb7f9d605655be9be8585307d55151
Jason Uithol <jasonuithol@gmail.com>
2026-04-08 19:13:58 +1000

    make project a parameter

--- run-valheim.sh
+++ run-valheim.sh
@@ -1,3 +1,5 @@
+PROJECT="$1"
+
 podman run -it --rm \
   -v ~/.claude:/root/.claude:Z \
   -v ~/.claude.json:/root/.claude.json:Z \
@@ -5,6 +7,6 @@ podman run -it --rm \
   -v "$HOME/.steam/steam/steamapps/common/Valheim dedicated server":/workspace/valheim/server:Z \
   -v "$HOME/.steam/steam/steamapps/common/Valheim":/workspace/valheim/client:Z \
   -v ~/Projects/claude-sandbox/VALHEIM.md:/workspace/VALHEIM.md:Z \
-  -v ~/Projects/ValheimRainDance:/workspace/ValheimRainDance:Z \
-  -w /workspace \
+  -v ~/Projects/$PROJECT:/workspace/$PROJECT:Z \
+  -w /workspace/$PROJECT \
   claude-sandbox
